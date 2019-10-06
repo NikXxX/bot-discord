46143d020e6c398e89e5085aab7878baee4e01e6
CosmoXxX <50768054+CosmoXxX@users.noreply.github.com>
2019-10-06 13:20:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  TOKEN: "NjI5NTQ4NDU3MjQxMjE0OTg2.XZm80w.IVnIwuO3jpwF774TfT0GPKZ6oSo",
+  TOKEN: "",
   PREFIX: "*",
-  DBCONNECTION: "mongodb://localhost:27017/drackers",
-  DEFAULTSETTINGS: {
-    prefix: "*"
-  }
+ 
 };
