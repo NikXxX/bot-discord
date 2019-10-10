e6a5cea6c815ab4292a0d6212bf5300528a73563
NikXxX <52099393+NikXxX@users.noreply.github.com>
2019-10-10 18:14:30 +0400

    Update beautiful.js

--- commands/beautiful.js
+++ commands/beautiful.js
@@ -1,13 +1,14 @@
 module.exports.run = async (client, message, args) => {
   let user = message.mentions.users.first() || message.author;
-  let avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=2048`;
+  //let avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=2048`;
 
   const ameClient = require("amethyste-api");
   const ameApi = new ameClient(
     "ff41a095bc493607d2865295c74cf72329fa9681b5fb57c08bae6493d550d7bc3c0433508e04dfd3f2c569d888b8ae33e20529fb1489b6b2e25f92226b6ef1d2"
   );
   let image = ameApi
-    .generate("beautiful", { url: avatar })
+    .generate("beautiful", { url: .displayAvatarURL({ size: 1024,format : "png" }) 
+})
     .then(image => {
 
       message.channel.send({
